835c7370335733bf97a5a897e8d703de8f83c2df
Sunil Wang <ahwangshu@qq.com>
2017-04-28 10:09:54 +0800

    extended function

--- example.js
+++ example.js
@@ -2,11 +2,41 @@ let serverTimestamp = require('./');
 let koa = require('koa');
 let app = koa();
 
+//default
 app.use(serverTimestamp());
+
+//set header
+app.use(serverTimestamp({header: 'Example-Server-Timestamp'}));
+
+//set header and format
+app.use(serverTimestamp({
+    header: 'Example-Format-Server-Timestamp',
+    format: (timestamp) => {
+        let now = new Date(timestamp);
+        let year = now.getFullYear();
+        let month = now.getMonth() + 1;
+        let date = now.getDate();
+        let hour = now.getHours();
+        let minute = now.getMinutes();
+        let second = now.getSeconds();
+
+        return `${year}-${month}-${date} ${hour}:${minute}:${second}`;
+    }
+}));
 
 app.use(function *(next){
     yield next;
-    this.body = this.response.get('X-Server-Timestamp');
+    /*
+     Body results:
+
+     {
+        "x-server-timestamp": "1493344844811",
+        "example-server-timestamp": "1493344844812",
+        "example-format-server-timestamp": "2017-4-28 10:3:44",
+        "content-type": "application/json; charset=utf-8"
+     }
+     */
+    this.body = this.response.header;
 });
 
 app.listen(3000);
--- index.js
+++ index.js
@@ -1,19 +1,66 @@
+/*!
+ * response-time
+ * Copyright(c) 2017-2017 Sunil Wang
+ * MIT Licensed
+ */
+
 /**
- * Expose `serverTimestamp()`.
+ * Module exports.
+ * @public
  */
 
 module.exports = serverTimestamp;
 
 /**
- * Add X-Server-Timestamp header field.
+ * Format timestamp, returns defaults to timestamp
  *
- * @return {Function}
+ * ex：
+ *
+ * function format(timestamp) {
+ *    let now = new Date(timestamp);
+ *    let year = now.getFullYear();
+ *    let month = now.getMonth() + 1;
+ *    let date = now.getDate();
+ *    let hour = now.getHours();
+ *    let minute = now.getMinutes();
+ *    let second = now.getSeconds();
+ *
+ *    //2017-4-27 18:36:56
+ *    return `${year}-${month}-${date} ${hour}:${minute}:${second}`;
+ * }
+ *
+ * @param {Number} timestamp
+ * @returns {Number|String|*} defaults to timestamp
+ * @private
+ */
+function defaultFormat(timestamp) {
+    return timestamp;
+}
+
+/**
+ * Create a middleware to add a server timestamp header in milliseconds.
+ * Use for Koa v1
+ *
+ * @param {String} [header] Format timestamp, see below for private function _format
+ * @param {Function} [format] The name of the header to set, defaults to X-Server-Timestamp.
+ * @returns {Function}
  * @api public
  */
+function serverTimestamp({header = 'X-Server-Timestamp', format = defaultFormat} = {}) {
+    if (typeof header !== 'string') {
+        throw new Error('header must be a string');
+    }
 
-function serverTimestamp() {
+    if (header.length < 1) {
+        throw new Error('header the length must be greater than 0');
+    }
+
+    if (typeof format !== 'function') {
+        throw new Error('format must be a function');
+    }
+
     return function * serverTimestamp(next){
-        this.set('X-Server-Timestamp', Date.now());
+        this.set(header, format(Date.now()));
         yield next;
     };
 }
